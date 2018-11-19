32103ab54fd24effc64b1bf81ca8e44defcda177
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook-Pro.local>
2018-11-19 12:48:44 -0700

    adding devtools

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(),
+  // other store enhancers if any
+));
 
 
 
