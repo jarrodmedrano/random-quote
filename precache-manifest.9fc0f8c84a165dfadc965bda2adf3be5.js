c2df19892383c452493b7b6ad3a9075513b3e89b
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook-Pro.local>
2018-12-16 17:48:52 -0700

    Updates

---
+++ precache-manifest.9fc0f8c84a165dfadc965bda2adf3be5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e95d36543807e9d62d34",
+    "url": "/randomquote/static/css/main.dd07552a.chunk.css"
+  },
+  {
+    "revision": "e95d36543807e9d62d34",
+    "url": "/randomquote/static/js/main.e95d3654.chunk.js"
+  },
+  {
+    "revision": "31f83e1282bbb35a3407",
+    "url": "/randomquote/static/js/1.31f83e12.chunk.js"
+  },
+  {
+    "revision": "539267bcd3a0a4e0f660",
+    "url": "/randomquote/static/js/runtime~main.539267bc.js"
+  },
+  {
+    "revision": "b2f948234f37aaf680dd3bd395f6ccb6",
+    "url": "/randomquote/index.html"
+  }
+];
